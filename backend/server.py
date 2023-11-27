b55d4101b3f8c90a435c09145117b51eff599664
Minh Ha <21020621@vnu.edu.vn>
2023-11-27 12:02:37 +0700

    add

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,25 @@
-from flask import Flask
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, jsonify
+from wsgiref.simple_server import WSGIServer
+import logging
+
+import model
 
 app = Flask(__name__)
+
 
 @app.route('/api', methods=['GET'])
 def index():
     return "Hello, World!"
+
+
+@app.route('/api/summarize', methods=['POST'])
+def summarize():
+    input_text = request.json['content']
+    summary = model.summarize(input_text)
+    response = jsonify({'summary': summary})
+    logging.log(logging.INFO, 'Response:', response)
+    return response
+
 
 if __name__ == '__main__':
     # Debug/Development
