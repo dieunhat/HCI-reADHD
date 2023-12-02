456825c1d6b1b5158669dcb38e5ae0626f62ad9b
Vu Bao Chau <baochauvu124@gmail.com>
2023-12-02 18:12:29 +0700

    actually highlight sth

--- frontend/src/features/reading/components/Highlight.js
+++ frontend/src/features/reading/components/Highlight.js
@@ -1,47 +1,20 @@
-import Highlight from 'react-highlight';
-
-window.onload=function(){
-    const HIGHLIGHT_BUTTON = document.querySelector("#highlight");
-    const CLEAR_BUTTON = document.querySelector("#clear")
-
-    HIGHLIGHT_BUTTON.addEventListener("click", () => textHighlight())
-    //CLEAR_BUTTON.addEventListener("click", () => clearHighlight())
-}
-let SELECTION_TEXTS = []
-let selectionHighlighter = new Highlight();
-
-function textHighlight(){
-    
-    // const HIGHLIGHT_BUTTON = document.querySelector("#highlight");
-    // const CLEAR_BUTTON = document.querySelector("#clear")
-    
+function Highlighter() {
     const textHighlight = () => {
         const selection = document.getSelection();
-        const range = selection.getRangeAt(0)
-        SELECTION_TEXTS.push(range)
-        
-        SELECTION_TEXTS.forEach((selection) => {
-            selectionHighlighter.add(selection)
-        })
-        CSS.highlights.set("example", selectionHighlighter)
-        };
-// }
+        const range = selection.getRangeAt(0);
+        const selectedText = range.toString();
+        range.deleteContents();
+        const span = document.createElement("span");
+        span.style.backgroundColor = "yellow";
+        span.appendChild(document.createTextNode(selectedText));
+        range.insertNode(span);
+    };
 
-// function clearHighlight(){
-//     const clearHighlight = () => {
-//         selectionHighlighter.clear()
-//         SELECTION_TEXTS = []
-//         CSS.highlights.clear()
-//     }
-      
-    // HIGHLIGHT_BUTTON.addEventListener("click", () => textHighlight())
-        
-    // CLEAR_BUTTON.addEventListener("click", () => clearHighlight())
     return (
         <>
-            <button className="btn">Highlight</button>
+            <button className="btn" id='highlight' onClick={textHighlight}>Highlight</button>
         </>
     )
-};
+}
 
-export default textHighlight;
+export default Highlighter;
