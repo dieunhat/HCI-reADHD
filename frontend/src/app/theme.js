5e33989324710ed4633dbb290799e1e56fc85fcd
Minh Ha <21020621@vnu.edu.vn>
2023-12-04 14:25:00 +0700

    theme

--- frontend/src/app/theme.js
+++ frontend/src/app/theme.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { useState } from 'react'; 
+import React from "react";
+import { useState, useEffect } from "react";
 
-const ThemeContext = React.createContext('light-theme'); // 'light' is the default value
+const ThemeContext = React.createContext("light-theme"); // 'light' is the default value
 
 export function ThemeProvider({ children }) {
-    const [theme, setTheme] = useState('light-theme');
-  
+    const [theme, setTheme] = useState(() => {
+        const storedTheme = window.localStorage.getItem("theme");
+        return storedTheme ? storedTheme : "light-theme";
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem("theme", theme);
+    }, [theme]);
     return (
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        {children}
-      </ThemeContext.Provider>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
     );
-  }
+}
 
 export default ThemeContext;
